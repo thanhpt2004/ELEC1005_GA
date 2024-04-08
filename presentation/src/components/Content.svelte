fc278466ba7a7313a3d86b0397b77a9f4e9928b3
lawlondon <londonalexanderw@gmail.com>
2024-04-08 16:50:01 +1000

    Added space for presentation content

--- presentation/src/components/Content.svelte
+++ presentation/src/components/Content.svelte
@@ -3,7 +3,23 @@
 </script>
 
 <SectionWrapper>
-  <div>
-    <h1 class="text-8xl">Content</h1>
+  <div class="flex flex-row justify-center p-4">
+    <div class="flex flex-col flex-grow">
+      <h3 class="text-4xl text-left pb-4">Subheading</h3>
+      <p class="text-2xl text-justify">
+        A sample paragraph of text that is very long. It contains a lot of words
+        becuase this is a template I am making and I want there to be a complete
+        paragrah here on my website. It is currently 4:45 pm and it has been a
+        nice day.
+      </p>
+    </div>
+
+    <div>
+      <img
+        src="https://expertphotography.b-cdn.net/wp-content/uploads/2019/03/yns-plt-527773-unsplash-1.jpg"
+        alt="Goofy Man"
+        class="flex-shrink-0"
+      />
+    </div>
   </div>
 </SectionWrapper>
--- presentation/src/components/Hero.svelte
+++ presentation/src/components/Hero.svelte
@@ -5,7 +5,7 @@
 
 <SectionWrapper>
   <Header />
-  <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10">
+  <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-4">
     <h2 class="text-8xl font-semibold">Main Point of Presentation.</h2>
     <p class="text-4xl">We also have other points besides the main point!</p>
     <p class="text-2xl">We have so many points...</p>
